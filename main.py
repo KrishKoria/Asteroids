5cdf0af15c14e223701838d40e847f0d0971fc83
Krish Koria <krishkoria2004@gmail.com>
2025-01-06 15:30:26 +0000

    Add player movement functionality and update game loop structure

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pygame
 from constants import *
 from player import *
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    
     clock = pygame.time.Clock()
     dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x=(SCREEN_WIDTH/2), y=(SCREEN_HEIGHT/2))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.add(updatable, drawable)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
-        pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)     
+        for item in updatable:
+            item.update(dt)
+        screen.fill((0, 0, 0))
+        for item in drawable:
+            item.draw(screen)     
         pygame.display.update()
         dt = clock.tick(60) * 0.001 
-
 
 if __name__ == "__main__":
     main()
